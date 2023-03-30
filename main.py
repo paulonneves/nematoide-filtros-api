890ab5118f9652671ad94dfbc0a32c3263b147b4
ricardo <ricardp.fn@gmail.com>
2023-03-30 20:50:32 -0300

    Api servindo imagens com filtros

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from typing import Annotated
+from os import listdir, path
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, StreamingResponse
+from PIL import Image
+from io import BytesIO
 
-from fastapi import FastAPI, File, UploadFile
+from lib import apply_filters
 
 app = FastAPI()
+a_path = 'C:\\Users\\ricar\\Documents\\code\\nametoide'
+list_filters = ['', 'canny', 'lighten', 'sobel', 'prewitt', 'roberts', 'scharr']
 
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+@app.get("/images")
+async def create_upload_file(nameimage, namefilter: str = ''):
+    list_images = listdir('samples')
+    if nameimage in list_images and namefilter in list_filters:
+        r_path = path.join(a_path, f'samples\\{nameimage}')
+        if namefilter == '':
+            return FileResponse(r_path)
+        img_filtered = apply_filters(r_path, namefilter)
+        pil_image_filtered = Image.fromarray(img_filtered)
+        buffer = BytesIO()
+        pil_image_filtered.save(buffer, "JPEG")
+        buffer.seek(0)
+        return StreamingResponse(buffer, media_type="image/jpeg")
+    return {'error': 'image file not found'}
